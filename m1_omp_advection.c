8eaf2b9ab1e1a7942050d99ca780a5d4d2fc0f98
Nirman Gupta <guptanirman11@linux6.cs.uchicago.edu>
2024-04-17 23:26:27 -0500

    parallel done

--- m1_omp_advection.c
+++ m1_omp_advection.c
@@ -3,6 +3,10 @@
 #include <math.h>
 #include <omp.h>
 
+// Function prototypes
+void update_concentration(double **C, double **C_new, int N, double dt, double dx, double dy, double u, double v);
+void apply_boundary_conditions(double **C, double **C_new, int N);
+
 int main(int argc, char *argv[]){
     if (argc != 8){
         printf("Usage: %s N NT L T u v\n", argv[0]);
@@ -44,7 +48,9 @@ int main(int argc, char *argv[]){
     double y_0 = L/2;
     double sigma = L/4;
 
-    #pragma omp parallel for default(none) shared(C, N, dx, dy, x_0, y_0, sigma) private(i, j)
+    double start_time = omp_get_wtime();
+
+    #pragma omp parallel for default(none) shared(C, N, dx, dy, x_0, y_0, sigma)
     // Initialize concentration array
     for (int i = 0;i < N; i++){
         for (int j = 0; j<N; j++){
@@ -54,45 +60,22 @@ int main(int argc, char *argv[]){
         }
     }
 
+    
     // Time loop and boundary conditions
     for (int t = 0; t < NT; t++){
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                // Have to calculate first and then apply boundary conditions
-                int im1 = (i == 0) ? N-1 : i-1;
-                int ip1 = (i == N-1) ? 0 : i+1;
-                int jm1 = (j == 0) ? N-1 : j-1;
-                int jp1 = (j == N-1) ? 0 : j+1;
-
-                C_new[i][j] = 0.25 * (C[im1][j] + C[ip1][j] + C[i][jm1] + C[i][jp1])
-                            - dt / (2 * dx) * (u * (C[ip1][j] - C[im1][j]) + v * (C[i][jp1] - C[i][jm1]));
         
-            }
-        
-        }
-        // Applying Boundary conditions
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                if (i == 0){
-                    C_new[i][j] = C_new[N-1][j];
-                }
-                if (i == N-1){
-                    C_new[i][j] = C_new[0][j];
-                }
-                if (j == 0){
-                    C_new[i][j] = C_new[i][N-1];
-                }
-                if (j == N-1){
-                    C_new[i][j] = C_new[i][0];
-                }
-            }
-
-        }
+        update_concentration(C, C_new, N, dt, dx, dy, u, v);
+     
+        apply_boundary_conditions(C, C_new, N);
         // Swap pointers
         double **tmp = C;
         C = C_new;
         C_new = tmp;
     }
+    double end_time = omp_get_wtime();
+
+    printf("Time taken: %f\n", end_time - start_time);
+    
     // Free memory
     for (int i = 0; i < N; i++){
         free(C[i]);
@@ -104,4 +87,42 @@ int main(int argc, char *argv[]){
     
     return 0;
 
+}
+
+
+void update_concentration(double **C, double **C_new, int N, double dt, double dx, double dy, double u, double v){
+    int i, j;
+    #pragma omp parallel for default(none) shared(C, C_new, N, dt, dx, u, v) private(i, j)
+    for (int i = 0; i <N;i++){
+        for (int j = 0;j<N;j++){
+            int im1 = (i == 0) ? N-1 : i-1;
+            int ip1 = (i == N-1) ? 0 : i+1;
+            int jm1 = (j == 0) ? N-1 : j-1;
+            int jp1 = (j == N-1) ? 0 : j+1;
+
+            C_new[i][j] = 0.25 * (C[im1][j] + C[ip1][j] + C[i][jm1] + C[i][jp1])
+                        - dt / (2 * dx) * (u * (C[ip1][j] - C[im1][j]) + v * (C[i][jp1] - C[i][jm1]));
+        }
+    }
+}
+
+void apply_boundary_conditions(double **C, double **C_new, int N){
+    int i, j;
+    #pragma omp parallel for default(none) shared(C, C_new, N) private(i, j)
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (i == 0){
+                C_new[i][j] = C_new[N-1][j];
+            }
+            if (i == N-1){
+                C_new[i][j] = C_new[0][j];
+            }
+            if (j == 0){
+                C_new[i][j] = C_new[i][N-1];
+            }
+            if (j == N-1){
+                C_new[i][j] = C_new[i][0];
+            }
+        }
+    }
 }
